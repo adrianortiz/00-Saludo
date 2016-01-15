b45352734d2c5b8eaa1166b3d2b858517d6348b3
Codizer <adrianortizmartinez@gmail.com>
2016-01-15 02:13:43 -0600

    Saludo
    Saludo en Java

---
+++ src/Saludo.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ * @author Adrian
+ *
+ */
+public class Saludo {
+
+	/**
+	 * Programa que muestra un mensjae en consola
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Hola Adrian");
+		System.out.println("Hola desde la consola");
+
+	}
+
+}
