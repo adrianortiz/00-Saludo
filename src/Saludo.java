fc22adcda7635d6c191c9ad8aee4b3d8c97e8c4e
Codizer <adrianortizmartinez@gmail.com>
2016-01-18 19:25:47 -0600

    Create diagram

--- src/Saludo.java
+++ src/Saludo.java
@@ -5,13 +5,15 @@ import javax.swing.JOptionPane;
  * @author Adrian
  *
  */
-public class Saludo {
+public class Saludo 
+{
 
 	/**
 	 * Programa que muestra un mensaje de saludo en un alert y consola
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		JOptionPane.showMessageDialog(null, "Hola Adrian");
 		System.out.println("Hola desde la consola");
